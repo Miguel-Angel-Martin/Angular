da0dcdc0b0a1999e7789bb401881ff4db2fdc74d
Miguel-Angel-Martin <miguel-angel.martin@avl.com>
2021-03-02 10:07:43 +0100

    lodash

--- src/app/Lodash/Lodash.component.ts
+++ src/app/Lodash/Lodash.component.ts
@@ -1,18 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  find,
-  pull,
-  filter,
-  times,
-  constant,
-  debounce,
-  set,
-  get,
-  keyBy,
-  reduce,
-  cloneDeep,
-  sortedUniq,
-} from 'lodash';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-lodash',
   templateUrl: './Lodash.component.html',
@@ -22,7 +9,7 @@ export class LodashComponent {
   rating: string;
   movieExist: boolean;
   movieNotExist: boolean;
-
+  hellos = [];
   movies = [
     { name: 'Batman Begins', rating: 8.2 },
     { name: 'Insomnia', rating: 7.1 },
@@ -37,20 +24,25 @@ export class LodashComponent {
   sortedNums = [11, 13, 21, 3, 3, 15, 18, 8, 13];
 
   constructor() {
-    const findValue = find(this.movies, (movie) => (movie.rating = 7.1));
+    const findValue = _.find(this.movies, (movie) => (movie.rating = 7.1));
     console.log(findValue);
-    this.movieExist = !!find(this.movies, { name: 'Batman Begins' });
-    this.movieNotExist = !!find(this.movies, { name: 'ABABAB' });
 
-    console.log(times(3, String));
-    console.log(times(4, constant('edupala')));
-    debounce(this.callMethods, 5000)();
-    set(this.names, '4', 'Bootstrap');
+    this.movieExist = !!_.find(this.movies, { name: 'Batman Begins' });
+
+    this.movieNotExist = !!_.find(this.movies, { name: 'ABABAB' });
+
+    console.log(_.times(3, String));
+    console.log(_.times(4, _.constant('edupala')));
+
+    _.debounce(this.callMethods, 5000)();
+
+    _.set(this.names, '4', 'Bootstrap');
+
     console.log(this.names);
-    console.log('Get movie name', get(this.movies, 'name', 'Doodlebug'));
-    console.log(keyBy(this.movies, 'rating'));
+    console.log('Get movie name', _.get(this.movies, 'name', 'Doodlebug'));
+    console.log(_.keyBy(this.movies, 'rating'));
 
-    const reducedMovie = reduce(
+    const reducedMovie = _.reduce(
       this.movies,
       (result, movie) => {
         if (movie.rating >= 7 && movie.rating <= 9.1) {
@@ -63,21 +55,35 @@ export class LodashComponent {
     );
 
     console.log('Reduce : ', reducedMovie);
-    const copy = cloneDeep(this.movies);
+    const copy = _.cloneDeep(this.movies);
     console.log('This copy method of lodash : ' + copy);
-    console.log('Sort and remove duplicate', sortedUniq(this.sortedNums));
+    console.log('Sort and remove duplicate', _.sortedUniq(this.sortedNums));
   }
 
   findMoviesByRating(rating): void {
-    this.moviesByRating = filter(this.movies, (movie) => movie.rating > rating);
+    this.moviesByRating = _.filter(
+      this.movies,
+      (movie) => movie.rating > rating
+    );
   }
 
   removeItem(item: string): void {
-    this.names = pull(this.names, item);
+    this.names = _.pull(this.names, item);
     console.log(this.names);
   }
 
   callMethods(): void {
     alert('Call after 5 second');
   }
-}
+
+  render() {
+    _.times(2, () => {
+      this.hellos.push('Hello');
+      console.log('render', this.hellos);
+    });
+    _.range(4);
+    console.log(_.range(4));
+
+    // return this.hellos;
+  }
+} // end of class
