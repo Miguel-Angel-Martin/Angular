2a1a1e8c30c172e31210db0de2e99c670a6bf353
Miguel-Angel-Martin <miguel-angel.martin@avl.com>
2021-03-05 14:49:25 +0100

    mas componentes de angular material

--- src/app/components/products/add/add.component.ts
+++ src/app/components/products/add/add.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  styleUrls: ['./add.component.css'],
 })
 export class AddComponent implements OnInit {
+  panelOpenState = false;
 
-  constructor() { }
+  hidden = false;
 
-  ngOnInit(): void {
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
   }
+  counter: number = 1;
 
+  addCounter() {
+    this.counter = this.counter + 1;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -6,9 +6,9 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ListComponent } from '../../components/products/list/list.component';
 import { AddComponent } from '../../components/products/add/add.component';
 import { DetailComponent } from '../../components/products/detail/detail.component';
-
+import { MaterialModules } from '../../../services/material.module';
 @NgModule({
   declarations: [ListComponent, AddComponent, DetailComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [CommonModule, ProductsRoutingModule, MaterialModules],
 })
 export class ProductsModule {}
